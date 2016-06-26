63187a32f2aca90202f0c6f39b04c4568e952492
fgliu <fgliu@live.cn>
2016-06-26 18:36:53 +0800

    数据库相关操作类

---
+++ app/src/main/java/com/fgliu/coolweather/model/Country.java
@@ -0,0 +1,44 @@
+package com.fgliu.coolweather.model;
+
+/**
+ * Created by andy on 16/6/26.
+ */
+public class Country {
+
+    private int id;
+    private String countryName;
+    private String countryCode;
+    private int city_id;
+
+    public int getCity_id() {
+        return city_id;
+    }
+
+    public void setCity_id(int city_id) {
+        this.city_id = city_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+}
